8e6c3b6947cd88a65ee8aad930838179ed3940fa
LuizaTeodoraRusu <luizarusuteodora@gmail.com>
2023-10-22 16:18:29 +0300

    Add project files.

---
+++ Rusu_Luiza_Lab2/Data/Rusu_Luiza_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rusu_Luiza_Lab2.Models;
+
+namespace Rusu_Luiza_Lab2.Data
+{
+    public class Rusu_Luiza_Lab2Context : DbContext
+    {
+        public Rusu_Luiza_Lab2Context (DbContextOptions<Rusu_Luiza_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rusu_Luiza_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Rusu_Luiza_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
---
+++ Rusu_Luiza_Lab2/Migrations/20231019202408_PublishingDateUpdated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rusu_Luiza_Lab2.Migrations
+{
+    public partial class PublishingDateUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Book",
+                type: "decimal(6,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Book",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(6,2)");
+        }
+    }
+}
